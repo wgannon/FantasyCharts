057ca2168963e05b9c3f8099a5f796ad9e3e7f52
Will Gannon <42075666+wgannon@users.noreply.github.com>
2025-11-24 16:47:54 -0500

    Update next.config.ts
    Updated next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
+const repo = "https://github.com/wgannon/FantasyCharts";
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   /* config options here */
 };
 
