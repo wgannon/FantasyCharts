d1ae0c83aa434f18169df321f32a257e8d116044
Will Gannon <wgannon@wisc.edu>
2025-11-25 15:34:41 -0500

    Updated and run static page

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const repo = "https://wgannon.github.io/FantasyCharts/";
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/FantasyCharts",
   /* config options here */
 };
+module.exports = nextConfig;
 
-export default nextConfig;
